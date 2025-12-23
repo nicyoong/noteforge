fda28fc1e8b18cb3c90417acf3f1992258231217
Nic <73386479+nicyoong@users.noreply.github.com>
2025-12-23 22:42:41 +0800

    left

--- ui.py
+++ ui.py
@@ -41,3 +41,35 @@ class MainWindowUI:
 
         self.splitter = QSplitter(Qt.Horizontal)
         root.addWidget(self.splitter)
+
+        # Left: list + search/filter
+        left = QWidget()
+        left_layout = QVBoxLayout(left)
+        left_layout.setContentsMargins(0, 0, 0, 0)
+        left_layout.setSpacing(6)
+
+        self.search = QLineEdit()
+        self.search.setPlaceholderText("Search (FTS5)…")
+        left_layout.addWidget(self.search)
+
+        self.tag_filter = QLineEdit()
+        self.tag_filter.setPlaceholderText("Tag filter (substring)…")
+        left_layout.addWidget(self.tag_filter)
+
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        self.table.verticalHeader().setVisible(False)
+        left_layout.addWidget(self.table, 1)
+
+        btn_row = QHBoxLayout()
+        self.btn_new = QPushButton("New")
+        self.btn_delete = QPushButton("Delete")
+        btn_row.addWidget(self.btn_new)
+        btn_row.addWidget(self.btn_delete)
+        btn_row.addStretch(1)
+        left_layout.addLayout(btn_row)
+        
